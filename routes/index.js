ea4f68e610452cfccd58f3a8d016a7dba5cdb218
ARNABGHOSH123 <36646193+ARNABGHOSH123@users.noreply.github.com>
2019-07-06 21:10:10 +0530

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -10,101 +10,8 @@ var dbname='myproject';
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  //console.log(req.session.success);
-  // res.render('index', { title: 'Form Validation',success: req.session.success,errors: req.session.errors });
-  // req.session.errors=null;
-  // req.session.success=null;
-
   res.render('index',{chosenLanguage:'java',sourceCode:'',output:null});
 });
-
-// router.get('/get-data',function(req,res,next){
-//   let resultArray=[];
-//   mongodb.connect(mongoUrl,function(error,client){
-//     assert.equal(null,error);
-//     let db=client.db(dbname);
-//     let cursor=db.collection('user-data').find();
-//     cursor.forEach(function(doc,err){
-//       assert.equal(null,err);
-//       resultArray.push(doc);
-//     }, function(){
-//       res.render('index',{items: resultArray});
-//     })
-//   })
-
-// });
-
-// router.post('/insert',function(req,res,next){
-
-//   let item={
-//     title:req.body.title,
-//     content:req.body.content,
-//     author:req.body.author
-//   };
-
-//   mongodb.connect(mongoUrl, function(error,client){
-//     assert.equal(null,error);
-//     let db=client.db(dbname);
-//     console.log('connected');
-//     db.collection('user-data').insertOne(item,function(error,result){
-//       assert.equal(null,error);
-//       console.log('Item successfully inserted');
-//     })
-//   })
-//   res.redirect('/');
-
-// });
-
-// router.post('/update',function(req,res,next){
-//   let item={
-//     title:req.body.title,
-//     content:req.body.content,
-//     author:req.body.author
-//   }
-//   let id=req.body.id;
-
-//   mongodb.connect(mongoUrl,function(error,client){
-//     assert.equal(null,error);
-//     let db=client.db(dbname);
-//     db.collection('user-data').updateOne({"_id": objectId(id)},{$set: item},function(error,result){
-//       assert.equal(null,error);
-//       console.log('Item successfully updated');
-//     })
-//   })
-//   res.redirect('/');
-// });
-
-// router.post('/delete',function(req,res,next){
-//   mongodb.connect(mongoUrl,function(error,client){
-//     assert.equal(null,error);
-//     let db=client.db(dbname);
-//     db.collection('user-data').deleteOne({"_id": objectId(req.body.id)},function(error,result){
-//       assert.equal(null,error);
-//       console.log('Item successfully deleted');
-//     })
-//   })
-//   res.redirect('/');
-
-// });
-
-
-
-// router.post('/submit',function(req,res,next){
-//   req.check('email','Invalid email id').isEmail();
-//   req.check('password','Invalid password').isLength({min: 4}).equals(req.body['confirm_password']);
-
-//   let errors=req.validationErrors();
-//   //console.log(errors);
-//   if(errors){
-//     req.session.errors=errors;
-//     req.session.success=false;
-//   }
-//   else
-//   {
-//     req.session.success=true;
-//   }
-//   res.redirect('/');
-// })
 
 router.post('/submit-code',function(req,res,next){
   if(req.body.chosenLanguage=='java')
@@ -130,6 +37,7 @@ router.post('/submit-code',function(req,res,next){
   }
 });
 
+//For Java
 function runCode(fileName,req,res)
 {
   let compile=executeCode('javac '+fileName);
@@ -173,6 +81,8 @@ function runCode(fileName,req,res)
     });
   });
 }
+
+//For Python
 function runPythonCode(fileName,req,res){
   let output=executeCode('python '+fileName);
 
